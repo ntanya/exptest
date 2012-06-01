f41997c1a273b81a8815fc68f3f0e497fb541574
Tanya N <ntanya@gmail.com>
2012-06-01 15:31:53 -0400

    mongo working

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
-var ArticleProvider = require('./art-memory').ArticleProvider;
+//memory storage
+//var ArticleProvider = require('./art-memory').ArticleProvider;
+
+//mongo storage
+var ArticleProvider = require('./art-mongo').ArticleProvider;
 
 var app = module.exports = express.createServer();
 
@@ -21,23 +25,13 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-var articleProvider= new ArticleProvider();
+var articleProvider= new ArticleProvider('localhost',27017);
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
         res.render('index.jade', { locals: {
             title: 'Blog',
             articles:docs
-            }
-        });
-    })
-});
-
-app.get('/blog/:id', function(req, res){
-    articleProvider.findById(req.params.id, function(error,article){
-        res.render('blog_item.jade', { locals: {
-              title: article.title,
-              article:article
             }
         });
     })
@@ -60,50 +54,15 @@ app.post('/blog/new', function(req, res){
     });
 });
 
+app.get('/blog/:id', function(req, res){
+    articleProvider.findById(req.params.id, function(error,article){
+        res.render('blog_item.jade', { locals: {
+              title: article.title,
+              article:article
+            }
+        });
+    })
+});
+
+
 app.listen(3000);
-/**
- * Module dependencies.
- */
-/*
-var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
-var ArticleProvider = require('./art-memory').ArticleProvider;
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-//app.get('/', routes.index);
-
-app.get('/', function(req, res){
-  articleProvider.findAll(function(error, docs){
-     res.send(docs);
-  });
-});
-
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
-*/
