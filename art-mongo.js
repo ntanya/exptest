6888d778a0236f1f1a5a55aec7975660df4bcb61
Tanya N <ntanya@gmail.com>
2012-06-05 15:49:54 -0400

    ser

--- art-mongo.js
+++ art-mongo.js
@@ -13,21 +13,20 @@ var ObjectID = require('mongodb').ObjectID;
 //mongodb://heroku_app4943648:a522qdedvi1nm06g30ccb5jic9@ds033087.mongolab.com:33087/heroku_app4943648
 
 ArticleProvider = function() {
+	var connect = require('connect'),
+        mongo = require('mongodb');
 
+    // Connect to a mongo database via URI
+	// With the MongoLab addon the MONGOLAB_URI config variable is added to your
+	// Heroku environment.  It can be accessed as process.env.MONGOLAB_URI
+	mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db){
 
-var connect = require('connect'),
-  mongo = require('mongodb');
-
-// Connect to a mongo database via URI
-// With the MongoLab addon the MONGOLAB_URI config variable is added to your
-// Heroku environment.  It can be accessed as process.env.MONGOLAB_URI
-mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db){
-
-  // console.log will write to the heroku log which can be accessed via the 
-  // command line as "heroku logs"
-  db.addListener("error", function(error){
-    console.log("Error connecting to MongoLab");
-  });
+	  // console.log will write to the heroku log which can be accessed via the 
+	  // command line as "heroku logs"
+	  db.addListener("error", function(error){
+	    console.log("Error connecting to MongoLab");
+	  });
+	});
   
 
   //this.db= new Db('heroku_app4943648', new Server('heroku_app4943648:a522qdedvi1nm06g30ccb5jic9@ds033087.mongolab.com', 33087, {auto_reconnect: true}, {}));
